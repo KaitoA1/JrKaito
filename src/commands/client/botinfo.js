9f6f2bb68fa0435bbe7a02719ffc66e2e90e5da4
Matthew Nguyen <mattnguyen0401@gmail.com>
2020-05-13 21:57:00 -0700

    Alpha v1.0.0

---
+++ src/commands/client/botinfo.js
@@ -0,0 +1,15 @@
+module.exports = {
+    config: {
+        name: "N/A",
+        description: "N/A",
+        usage: "N/A",
+        category: "N/A",
+        accessableby: "N/A",
+        aliases: ["N/A"],
+        ownerOnly: N/A,
+        userPerms: [N/A], 
+        clientPerms: [N/A],
+    },
+    run: async (bot, message, args) => { 
+         }
+}
