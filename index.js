73b77a01e3b38cec641f82bab47ded40a6b3a2dd
Matthew Nguyen <mattnguyen0401@gmail.com>
2020-05-13 22:21:00 -0700

    Alpha v1.0.0

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const { Client, Collection } = require('discord.js');
 const bot = new Client();
 
 ['commands', 'aliases'].forEach(c => bot[c] = new Collection());
-['commandHandler', 'eventHandler'].forEach(h => require(`./utils/handlers/${h}`)(bot));
+['commandHandler', 'console', 'eventHandler'].forEach(h => require(`./utils/handlers/${h}`)(bot));
 
 bot.login(process.env.botTOKEN);
