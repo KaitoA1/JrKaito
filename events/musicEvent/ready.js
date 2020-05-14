6dcc3a787cd09db46717a6d80622b44edd97c805
Matthew Nguyen <mattnguyen0401@gmail.com>
2020-05-14 00:44:14 -0700

    Alpha v1.0.1

---
+++ events/musicEvent/ready.js
@@ -0,0 +1,26 @@
+const { nodes } = require('../../botConfig.json');
+
+const { ErelaClient } = require('erela.js');
+
+module.exports = async (bot) => {
+
+    bot.music = new ErelaClient(bot, nodes)
+
+    bot.music.on('nodeConnect', () => {
+        console.log("Node CONNECTED");
+    });
+
+    bot.music.on('nodeError', (error) => {
+        console.log(`Node error: ${error.message}`);
+    });
+
+    bot.music.on('trackStart', (player, track) => {
+        player.textChannel.send(`**Now playing:** ${track.title}`);
+    });
+
+    bot.music.on('queueEnd', (player) => {
+        player.textChannel.send("Queue has ended.");
+        bot.music.players.destroy(player.guild.id);
+    });
+
+}
