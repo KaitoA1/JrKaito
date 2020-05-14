a162eb0db01adbb41053026006415fdab6f3153f
Matthew Nguyen <mattnguyen0401@gmail.com>
2020-05-13 22:28:11 -0700

    Alpha v1.0.0

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,29 +1,15 @@
 const { botPREFIX } = require('../../botConfig.json');
 
 module.exports = async (bot, message) => {
-
+    
     if(message.author.bot || message.channel.type === "dm") return;
 
-    let args = message.content.slice(botPREFIX.length).trim().split(/ +/g);
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
 
-    if(!message.content.startsWith(botPREFIX)) return;
+    if(!message.content.startsWith(prefix)) return;
     let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
-
-    if (commandfile.config.ownerOnly && !owners.includes(message.author.id)) return message.channel.send("Sorry, you must be the bot owner to use this command")
-    let result = missingPerms(message.member, commandfile.config.userPerms)
-
-    if (commandfile.config.userPerms && !message.member.permissions.has(commandfile.config.userPerms)) return message.channel.send(`Sorry, you must have ${result} perms to run this command`)
-    result = missingPerms(message.guild.me, commandfile.config.clientPerms) // `Sorry, you must have ${result} perms to run this command`
-    if (commandfile.requirements.clientPerms && !message.guild.me.permissions.has(commandfile.config.clientPerms)) return message.channel.send(`Sorry, i must have ${result} perms to run this command`)
-    commandfile.run(client, message, args)
-    }
-    const missingPerms = (member, perms) => {
-    const missingPerms = member.permissions.missing(perms)
-        .map(str => `\`${str.replace(/_/g, ' ')}\``)
-
-    return missingPerms.length > 1 ? `${missingPerms.slice(0, -1).join(", ")} and ${missingPerms.slice(-1)[0]}` : missingPerms[0]
-
+    if(commandfile) commandfile.run(bot, message, args)
 
 
     // if (message.author.bot) return;
