7bf1e3092133fcdbbe2db5dbc44e0982b320cefc
Matthew Nguyen <mattnguyen0401@gmail.com>
2020-05-13 22:12:44 -0700

    Alpha v1.0.0

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,17 +1,16 @@
-const { botPREFIX } = require(`../../botConfig.json`);
+const { botPREFIX } = require('../../botConfig.json');
 
 module.exports = async (bot, message) => {
-
     if (message.author.bot) return;
     if (!message.guild) return;
     if (!message.content.startsWith(botPREFIX)) return;
     if (!message.member) message.member = await message.guild.fetchMember(message);
 
-    const args = message.content.slice(botPREFIX,this.length).trim().split(/ +/g);
+    const args = message.content.slice(botPREFIX.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
-
+    
     if (cmd.length === 0) return;
-    let command = bot.comand.get(cmd);
+    let command = bot.commands.get(cmd);
     if (!command) command = bot.commands.get(bot.aliases.get(cmd));
     if (command) command.run(bot, message, args);
 }
