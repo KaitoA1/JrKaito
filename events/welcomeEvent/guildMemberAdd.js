faaee433a1d8f62da81387847126a1262a44a6b8
Matthew <63630063+KaitoA1@users.noreply.github.com>
2020-05-15 01:44:55 -0700

    Alpha v1.0.2

--- events/welcomeEvent/guildMemberAdd.js
+++ events/welcomeEvent/guildMemberAdd.js
@@ -1,6 +1,73 @@
-module.exports = async (bot, member) => {   
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
-	if (!channel) return;
+const { MessageAttachment } = require('discord.js');
+const Canvas = require('canvas');
 
-	channel.send(`Welcome to the server, ${member}!`);
+module.exports = async (bot, member) => {   
+    const applyText = (canvas, text) => {
+        const ctx = canvas.getContext('2d');
+    
+        // Declare a base size of the font
+        let fontSize = 70;
+    
+        do {
+            // Assign the font to the context and decrement it so it can be measured again
+            ctx.font = `${fontSize -= 10}px sans-serif`;
+            // Compare pixel width of the text to the canvas minus the approximate avatar size
+        } while (ctx.measureText(text).width > canvas.width - 300);
+    
+        // Return the result to use in the actual canvas
+        return ctx.font;
+    };
+   
+   
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
+    if (!channel) return;
+    
+	// Set a new canvas to the dimensions of 700x250 pixels
+    const canvas = Canvas.createCanvas(700, 250);
+    
+	// ctx (context) will be used to modify a lot of the canvas
+    const ctx = canvas.getContext('2d');
+    
+	// Since the image takes time to load, you should await it
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	// This uses the canvas dimensions to stretch the image onto the entire canvas
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    
+    // Select the color of the stroke
+	ctx.strokeStyle = '#74037b';
+	// Draw a rectangle with the dimensions of the entire canvas
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+
+    // Pick up the pen
+	ctx.beginPath();
+	// Start the arc to form a circle
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	// Put the pen down
+	ctx.closePath();
+	// Clip off the region you drew on
+	ctx.clip();
+
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	// Move the image downwards vertically and constrain its height to 200, so it's a square
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	// Use helpful Attachment class structure to process the file for you
+	const attachment = new MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
 }
+
+
+
